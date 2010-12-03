f2139461899d0a08a36b4e6d38fc97b4e5c0032f
noqisofon <ned.rihihe@gmail.com>
2010-12-03 14:00:23 +0900

    first commit

---
+++ build/cs/util/Base64.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text;
+
+
+namespace spaghetti.util {
+
+
+    /// <summary>
+    /// 文字列を Base64 にエンコード、デコードするメソッドを提供します。
+    /// </summary>
+    public static class Base64 {
+        /// <summary>
+        /// 文字列を Base64 でエンコードします。
+        /// </summary>
+        /// <param name="original_text">エンコードしたい文字列。</param>
+        /// <returns>Base64 でエンコードされた文字列。</returns>
+        public static string encode64(string original_text) {
+            return encode64( original_text, Encoding.Default );
+        }
+        /// <summary>
+        /// 文字列を Base64 でエンコードします。
+        /// </summary>
+        /// <param name="original_text"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string encode64(string original_text, Encoding encoding) {
+            return Convert.ToBase64String( encoding.GetBytes( original_text ) );
+        }
+
+
+        /// <summary>
+        /// Base64 でエンコードされた文字列をデコードします。
+        /// </summary>
+        /// <param name="encoded_text">Base64 でエンコードされた文字列。</param>
+        /// <returns>デコードされた文字列。</returns>
+        public static string decode64(string encoded_text) {
+            return decode64( encoded_text, Encoding.Default );
+        }
+        /// <summary>
+        /// Base64 でエンコードされた文字列をデコードします。
+        /// </summary>
+        /// <param name="encoded_text"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string decode64(string encoded_text, Encoding encoding) {
+            byte[] bs = Convert.FromBase64String( encoded_text );
+
+            return encoding.GetString( bs, 0, bs.Length );
+        }
+    }
+
+
+}
---
+++ build/cs/util/XMLDocumentConvertor.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+
+namespace spaghetti.util {
+
+
+    /// <summary>
+    /// XML の文字列表現から、XmlDocument オブジェクトを作成します。
+    /// </summary>
+    public class XMLDocumentConvertor {
+        /// <summary>
+        ///
+        /// </summary>
+        public XMLDocumentConvertor() {
+            this.settings_ = new XmlReaderSettings();
+            this.settings_.NameTable = new NameTable();
+
+            XmlNamespaceManager manager = new XmlNamespaceManager( this.settings_.NameTable );
+            this.context_ = new XmlParserContext( this.settings_.NameTable,
+                                                  manager,
+                                                  "ja",
+                                                  XmlSpace.Default
+                                                  );
+        }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="xml_text_representation"></param>
+        /// <returns></returns>
+        public XmlDocument convert(string xml_text_representation) {
+            XmlReader reader = create_xml_reader( xml_text_representation );
+
+            return convert_xml_document( reader );
+        }
+
+
+        /// <summary>
+        /// XmlReader オブジェクトを作成して返します。
+        /// </summary>
+        /// <param name="xml_text_representation"></param>
+        /// <returns></returns>
+        private XmlReader create_xml_reader(string xml_text_representation) {
+            XmlReader reader = XmlReader.Create( new XmlTextReader( xml_text_representation,
+                                                                    XmlNodeType.Document,
+                                                                    this.context_
+                                                                    ),
+                                                 this.settings_
+                                                 );
+
+            return reader;
+        }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private XmlDocument convert_xml_document(XmlReader reader) {
+            XmlDocument document = new XmlDocument();
+            XmlNode current;
+            XmlAttribute attribute;
+            Stack<XmlNode> node_stack = new Stack<XmlNode>();
+
+            node_stack.Push( document );
+            while ( reader.Read() ) {
+                if ( reader.NodeType == XmlNodeType.Whitespace )
+                    continue;
+
+                switch ( reader.NodeType ) {
+                    case XmlNodeType.XmlDeclaration:
+                        if ( reader.HasAttributes ) {
+                            document.AppendChild( document.CreateXmlDeclaration( reader.GetAttribute( "version" ),
+                                                                                 reader.GetAttribute( "encoding" ),
+                                                                                 "no"
+                                                                                 )
+                                                  );
+                        }
+                        break;
+
+                    case XmlNodeType.Element:
+                        node_stack.Push( document.CreateElement( reader.Name ) );
+
+                        if ( reader.HasAttributes ) {
+                            current = node_stack.Peek();
+
+                            for ( int i = 0; i < reader.AttributeCount; ++i ) {
+                                reader.MoveToAttribute( i );
+
+                                attribute = document.CreateAttribute( reader.Name );
+                                attribute.Value = reader.Value;
+
+                                current.Attributes.Append( attribute );
+                            }
+                        }
+
+                        if ( reader.IsEmptyElement ) {
+                            foldElement( node_stack );
+                        }
+                        break;
+
+                    case XmlNodeType.Text:
+                        current = node_stack.Peek();
+                        current.AppendChild( document.CreateTextNode( reader.Value ) );
+                        break;
+
+                    case XmlNodeType.EndElement:
+                        foldElement( node_stack );
+                        break;
+                }
+            }
+            return document;
+        }
+
+
+        /// <summary>
+        /// スタックからノードを 2 つ取り出して畳み込みます。
+        /// </summary>
+        /// <param name="stack"></param>
+        private void foldElement(Stack<XmlNode> stack) {
+            XmlNode current, parent;
+
+            if ( stack.Count > 1 ) {
+                current = stack.Pop();
+                parent = stack.Pop();
+
+                parent.AppendChild( current );
+                //Console.WriteLine( "append {0} to {1}", parent.Name, current.Name );
+                stack.Push( parent );
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        XmlReaderSettings settings_;
+        /// <summary>
+        /// 
+        /// </summary>
+        XmlParserContext context_;
+    }
+
+
+}
